61c48cb80b5541eed567c0e14e5b4d9b40a4d20f
robedgar <116982166+robedgar@users.noreply.github.com>
2023-02-02 13:04:33 +0000

    Nav & Footer

--- features/common/modules/Navigation/components/NavigationMobile/NavigationMobile.js
+++ features/common/modules/Navigation/components/NavigationMobile/NavigationMobile.js
@@ -1,5 +1,61 @@
+import { HamburgerIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Menu,
+  Flex,
+  Text,
+  MenuList,
+  MenuItem,
+  MenuButton,
+  IconButton,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { HiHomeModern } from 'react-icons/hi2';
+import { navigationLinks } from '../../navigationConsts';
+
 const NavigationMobile = () => {
-  return <>NavigationMobile</>;
+  return (
+    <Box
+      color="blue.600"
+      padding="2rem"
+      backgroundColor="white"
+      display={{ base: 'block', md: 'none' }}
+    >
+      <Flex alignItems="center" justifyContent="space-between">
+        <Link href="/">
+          <Box display="flex" gap="2" alignItems="center">
+            <HiHomeModern size="30" />
+            <Text fontSize="2xl" fontWeight="black">
+              CASAZ
+            </Text>
+          </Box>
+        </Link>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<HamburgerIcon />}
+          />
+          <MenuList>
+            {navigationLinks.map((item) => (
+              <NavigationLink key={item.title} {...item} />
+            ))}
+          </MenuList>
+        </Menu>
+      </Flex>
+    </Box>
+  );
 };
 
 export default NavigationMobile;
+
+const NavigationLink = ({ title, link, icon }) => {
+  return (
+    <Link href={link}>
+      <MenuItem alignItems="center" gap="0.5rem">
+        {icon}
+        {title}
+      </MenuItem>
+    </Link>
+  );
+};
---
+++ features/common/modules/Navigation/navigationConsts.js
@@ -0,0 +1,11 @@
+import { HiPhoneArrowUpRight } from 'react-icons/hi2';
+
+export const navigationLinks = [
+  { title: 'Properties', link: '/properties' },
+  { title: 'Contact', link: '/contact' },
+  {
+    title: '(+44) 020 65345',
+    link: 'tel:+44 020 65345',
+    icon: <HiPhoneArrowUpRight />,
+  },
+];
